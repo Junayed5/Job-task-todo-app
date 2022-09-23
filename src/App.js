83f3342aa6d22d02fd9adb8382d7f74b5b118207
Junayed5 <mdjunayed@gmail.com>
2022-09-23 23:29:29 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="" >
+      <div className="hero min-h-screen" style={{ backgroundImage: `url("https://mobirise.com/blocks/assets/images/jumbotron178.jpg")` }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center">
+            <div className='flex gap-9 mt-[-700px]'>
+              <div className="card w-56 bg-base-100 shadow-xl">
+                <div className="card-body">
+                  <h1>Todo</h1>
+                </div>
+              </div>
+              <div className="card w-56 bg-base-100 shadow-xl">
+                <div className="card-body">
+                  <h1>Progress</h1>
+                </div>
+              </div>
+              <div className="card w-56 bg-base-100 shadow-xl">
+                <div className="card-body">
+                  <h1>Done</h1>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
